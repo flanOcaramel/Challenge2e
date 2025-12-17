517e1b42020a93b0072bf9f56789c859b157e2f8
Votre Nom <votre.email@exemple.com>
2025-12-17 12:25:25 +0100

    css fix play credits

--- src/View/home.php
+++ src/View/home.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Tailwind CSS -->
-    <link rel=\"stylesheet\" href=\"assets/css/tailwind-lite.css\">
+    <link rel="stylesheet" href="assets/css/tailwind-lite.css">
 
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
